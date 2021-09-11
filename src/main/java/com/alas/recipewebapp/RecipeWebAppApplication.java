f0307a6b2dc03faf6df42a410c5f63264227c31b
Fagnine <coulibalyfagnine@gmail.com>
2021-09-11 20:41:44 +0200

    I just have added the indexController

--- src/main/java/com/alas/recipewebapp/RecipeWebAppApplication.java
+++ src/main/java/com/alas/recipewebapp/RecipeWebAppApplication.java
@@ -1,13 +1,21 @@
 package com.alas.recipewebapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeWebAppApplication {
+public class RecipeWebAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeWebAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("recipe application by Fagninovic !!!");
+		
 	}
 
 }
